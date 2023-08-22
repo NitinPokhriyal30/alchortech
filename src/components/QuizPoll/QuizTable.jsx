fcfa54c3786f175e0a318aa44666e6facc074755
NitinPokhriyal30 <nitinpokhriyal30@gmail.com>
2023-08-22 13:32:33 +0530

    fix/ui for survey table

--- src/components/QuizPoll/QuizTable.jsx
+++ src/components/QuizPoll/QuizTable.jsx
@@ -1,15 +1,9 @@
 import React, { useState } from 'react'
-import {
-  AiOutlinePlus,
-  AiFillClockCircle,
-  AiFillCloseCircle,
-  AiFillRightCircle,
-  AiFillCaretDown,
-} from 'react-icons/ai'
+import { AiOutlinePlus, AiFillClockCircle, AiFillCloseCircle, AiFillRightCircle, AiFillCaretDown } from 'react-icons/ai'
 import { BsPencilFill } from 'react-icons/bs'
 import { RxCross1 } from 'react-icons/rx'
 import { Link } from 'react-router-dom'
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from 'react-router-dom'
 
 const SORT_OPTIONS = [
   { label: 'Last 60 days', value: '1' },
@@ -25,8 +19,8 @@ const QuizTable = () => {
   const navigate = useNavigate()
 
   return (
-    <div className='h-screen'>
-      <div className="mt-4 flex justify-between px-4">
+    <div className="h-screen">
+      <div className="mt-4 flex justify-between px-[25px]">
         <div className="font-Lato text-[20px] font-bold text-[#464646]">Quiz</div>
         <div className="rounded-md bg-[#5486E3] p-2 font-Lato text-white">
           <Link to="/quiz/create" className="flex items-center gap-1">
@@ -36,7 +30,7 @@ const QuizTable = () => {
         </div>
       </div>
 
-      <div className="mt-4 flex flex-wrap md:justify-start px-4 gap-y-4">
+      <div className="mt-4 flex flex-wrap gap-y-4 px-[25px] md:justify-start">
         <div className="flex flex-1 justify-around md:justify-start md:gap-6">
           <button className="flex items-center gap-2 text-[14px] text-[#8D8D8D]">
             <span>{<BsPencilFill />}</span>Draft
@@ -62,11 +56,7 @@ const QuizTable = () => {
             </button>
             <div className="absolute right-[1px] top-full z-10 hidden w-36 flex-col rounded-lg bg-white py-2 text-end drop-shadow-[0px_2px_6px_#44444F1A]  hover:flex peer-hover:flex child:cursor-pointer">
               {SORT_OPTIONS.map((option) => (
-                <p
-                  key={option.value}
-                  className=" px-4 py-1 font-Lato text-sm hover:bg-translucent-black"
-                  onClick={() => setSortBy(option)}
-                >
+                <p key={option.value} className=" px-4 py-1 font-Lato text-sm hover:bg-translucent-black" onClick={() => setSortBy(option)}>
                   {option.label}
                 </p>
               ))}
@@ -75,43 +65,34 @@ const QuizTable = () => {
         </div>
       </div>
 
-      <div className="my-1 px-4">
+      <div className="my-1 px-[25px]">
         <div className="h-[1px] w-full bg-[#cecece]"></div>
       </div>
 
-      <div className="mx-4 mt-2 rounded-lg bg-white drop-shadow-md">
+      <div className="mx-4 mt-2 rounded-[9px] bg-white drop-shadow-md">
         <table className="w-full">
           <thead>
-            <tr className="border-b border-[#cecece] ">
-              <th className="w-auto py-4 pl-8 md:pl-14 text-start font-Lato text-[16px] font-medium text-[#292929]">
-                Name
-              </th>
-              <th className="w-1/4 py-4 font-Lato text-[16px] font-medium text-[#292929]">
-                Start date
-              </th>
-              <th className="w-1/4 py-4 font-Lato text-[16px] font-medium text-[#292929]">
-                End date
-              </th>
-              <th className="w-1/4 py-4 font-Lato text-[16px] font-medium text-[#292929]">
-                Type
-              </th>
+            <tr className="border-b border-[#cecece] child:!py-[15.5px] child:!text-16px ">
+              <th className="w-auto py-[15.5px] pl-8 text-start font-Lato text-16px font-medium text-[#292929] md:pl-[45px]">Name</th>
+              <th className="w-1/4 py-4 font-Lato text-[16px] font-medium text-[#292929]">Start date</th>
+              <th className="w-1/4 py-4 font-Lato text-[16px] font-medium text-[#292929]">End date</th>
+              <th className="w-1/4 py-4 font-Lato text-[16px] font-medium text-[#292929]">Type</th>
+              <th className="w-fit py-4 text-right font-Lato text-[16px] font-medium text-[#292929]"></th>
             </tr>
           </thead>
           <tbody className="">
             {Array.from({ length: 5 }).map(() => (
-              <tr className="group rounded-xl border-b border-[#7B7B7B] hover:bg-[#ececec]" onClick={() => navigate("/survey/preview")}>
-                <td className="py-3 pl-8 md:pl-14 text-[16px] font-semibold text-[#5486E3]">Quiz 1</td>
-                <td className="py-3 text-center font-Lato text-[16px] font-normal text-[#292929]">
-                  Feb 13, 2023
+              <tr className="group rounded-xl hover:bg-[#ececec] child:border-b child:border-[#d1d1d1] child:py-[15px] child:text-16px" onClick={() => navigate('/survey/preview')}>
+                <td className="!border-0 !py-0 pl-8 pr-0 font-semibold text-[#5486E3] md:pl-[45px]">
+                  <span className="-mb-px inline-block w-full border-b border-inherit py-[15px]">Quiz 1</span>
                 </td>
-                <td className="py-3 text-center font-Lato text-[16px] font-normal text-[#292929]">
-                  Feb 18, 2023
-                </td>
-                <td className="py-3 text-center font-Lato text-[16px] font-normal text-[#292929]">
-                  Automatic
-                </td>
-                <td className="py-3 text-center opacity-0 transition-opacity duration-200 group-hover:opacity-100">
-                  <RxCross1 className="cursor-pointer text-[#292929]" />
+                <td className="text-center font-Lato font-normal text-[#292929]">Feb 13, 2023</td>
+                <td className="text-center font-Lato font-normal text-[#292929]">Feb 18, 2023</td>
+                <td className="text-center font-Lato font-normal text-[#292929]">Automatic</td>
+                <td className=" block !border-0 !py-0 pl-0 pr-8 font-semibold text-[#5486E3] md:pr-[45px]">
+                  <span className="-mb-px block w-full border-b border-inherit py-[15px] pr-4 ">
+                    <RxCross1 className="ml-auto h-[19px] cursor-pointer text-[#292929] opacity-0 group-hover:opacity-100" />
+                  </span>
                 </td>
               </tr>
             ))}
--- src/components/Survey/SurveryTable.jsx
+++ src/components/Survey/SurveryTable.jsx
@@ -1,15 +1,9 @@
 import React, { useState } from 'react'
-import {
-  AiOutlinePlus,
-  AiFillClockCircle,
-  AiFillCloseCircle,
-  AiFillRightCircle,
-  AiFillCaretDown,
-} from 'react-icons/ai'
+import { AiOutlinePlus, AiFillClockCircle, AiFillCloseCircle, AiFillRightCircle, AiFillCaretDown } from 'react-icons/ai'
 import { BsPencilFill } from 'react-icons/bs'
 import { RxCross1 } from 'react-icons/rx'
 import { Link } from 'react-router-dom'
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from 'react-router-dom'
 
 const SORT_OPTIONS = [
   { label: 'Last 60 days', value: '1' },
@@ -23,10 +17,12 @@ const SORT_OPTIONS = [
 const SurveyTable = () => {
   const [sortBy, setSortBy] = useState(SORT_OPTIONS[0])
   const navigate = useNavigate()
+  const [tab, setTab] = React.useState('draft')
+  const rows = tab === 'draft' ? 5 : tab === "running" ? 1 : tab === "closed" ? 8 : 7
 
   return (
-    <div className='h-screen'>
-      <div className="mt-4 flex justify-between px-4">
+    <div className="h-screen">
+      <div className="mt-4 flex justify-between px-[25px]">
         <div className="font-Lato text-[20px] font-bold text-[#464646]">Survey</div>
         <div className="rounded-md bg-[#5486E3] p-2 font-Lato text-white">
           <Link to="/survey/create" className="flex items-center gap-1">
@@ -36,18 +32,18 @@ const SurveyTable = () => {
         </div>
       </div>
 
-      <div className="mt-4 flex flex-wrap md:justify-start px-4 gap-y-4">
+      <div className="mt-4 flex flex-wrap gap-y-4 px-[25px] md:justify-start">
         <div className="flex flex-1 justify-around md:justify-start md:gap-6">
-          <button className="flex items-center gap-2 text-[14px] text-[#8D8D8D]">
+          <button className={'flex items-center gap-2 border-b-2 border-b-transparent text-[14px] text-[#8D8D8D] ' + (tab === 'draft' && '!border-primary  text-primary')} onClick={() => setTab('draft')}>
             <span>{<BsPencilFill />}</span>Draft
           </button>
-          <button className="flex items-center gap-2 text-[14px] text-[#8D8D8D]">
+          <button className={'flex items-center gap-2 border-b-2 border-b-transparent text-[14px] text-[#8D8D8D] ' + (tab === 'scheduled' && '!border-primary  text-primary')} onClick={() => setTab('scheduled')}>
             <span>{<AiFillClockCircle />}</span> Scheduled
           </button>
-          <button className="flex items-center gap-2 text-[14px] text-[#8D8D8D]">
+          <button className={'flex items-center gap-2 border-b-2 border-b-transparent text-[14px] text-[#8D8D8D] ' + (tab === 'running' && '!border-primary  text-primary')} onClick={() => setTab('running')}>
             <span>{<AiFillRightCircle />}</span>Running
           </button>
-          <button className="flex items-center gap-2 text-[14px] text-[#8D8D8D]">
+          <button className={'flex items-center gap-2 border-b-2 border-b-transparent text-[14px] text-[#8D8D8D] ' + (tab === 'closed' && '!border-primary  text-primary')} onClick={() => setTab('closed')}>
             <span>{<AiFillCloseCircle />}</span>Closed
           </button>
         </div>
@@ -62,11 +58,7 @@ const SurveyTable = () => {
             </button>
             <div className="absolute right-[1px] top-full z-10 hidden w-36 flex-col rounded-lg bg-white py-2 text-end drop-shadow-[0px_2px_6px_#44444F1A]  hover:flex peer-hover:flex child:cursor-pointer">
               {SORT_OPTIONS.map((option) => (
-                <p
-                  key={option.value}
-                  className=" px-4 py-1 font-Lato text-sm hover:bg-translucent-black"
-                  onClick={() => setSortBy(option)}
-                >
+                <p key={option.value} className=" px-4 py-1 font-Lato text-sm hover:bg-translucent-black" onClick={() => setSortBy(option)}>
                   {option.label}
                 </p>
               ))}
@@ -75,41 +67,27 @@ const SurveyTable = () => {
         </div>
       </div>
 
-      <div className="my-1 px-4">
+      <div className="mb-1 px-[25px]">
         <div className="h-[1px] w-full bg-[#cecece]"></div>
       </div>
 
       <div className="mx-4 mt-2 rounded-lg bg-white drop-shadow-md">
         <table className="w-full">
           <thead>
-            <tr className="border-b border-[#cecece] ">
-              <th className="w-auto py-4 pl-8 md:pl-14 text-start font-Lato text-[16px] font-medium text-[#292929]">
-                Name
-              </th>
-              <th className="w-1/4 py-4 font-Lato text-[16px] font-medium text-[#292929]">
-                Start date
-              </th>
-              <th className="w-1/4 py-4 font-Lato text-[16px] font-medium text-[#292929]">
-                End date
-              </th>
-              <th className="w-1/4 py-4 font-Lato text-[16px] font-medium text-[#292929]">
-                Type
-              </th>
+            <tr className="border-b border-[#cecece] child:text-16px ">
+              <th className="w-auto py-4 pl-8 text-start font-Lato font-medium text-[#292929] md:pl-14">Name</th>
+              <th className="w-1/4 py-4 font-Lato font-medium text-[#292929]">Start date</th>
+              <th className="w-1/4 py-4 font-Lato font-medium text-[#292929]">End date</th>
+              <th className="w-1/4 py-4 font-Lato font-medium text-[#292929]">Type</th>
             </tr>
           </thead>
           <tbody className="">
-            {Array.from({ length: 5 }).map(() => (
-              <tr className="group rounded-xl border-b border-[#7B7B7B] hover:bg-[#ececec]" onClick={() => navigate("/survey/preview")}>
-                <td className="py-3 pl-8 md:pl-14 text-[16px] font-semibold text-[#5486E3]">Survey 1</td>
-                <td className="py-3 text-center font-Lato text-[16px] font-normal text-[#292929]">
-                  Feb 13, 2023
-                </td>
-                <td className="py-3 text-center font-Lato text-[16px] font-normal text-[#292929]">
-                  Feb 18, 2023
-                </td>
-                <td className="py-3 text-center font-Lato text-[16px] font-normal text-[#292929]">
-                  Automatic
-                </td>
+            {Array.from({ length: rows }).map(() => (
+              <tr className="group rounded-xl border-b border-[#7B7B7B] hover:bg-[#ececec]" onClick={() => navigate('/survey/preview')}>
+                <td className="py-3 pl-8 text-[16px] font-semibold text-[#5486E3] md:pl-14">Survey 1</td>
+                <td className="py-3 text-center font-Lato text-[16px] font-normal text-[#292929]">Feb 13, 2023</td>
+                <td className="py-3 text-center font-Lato text-[16px] font-normal text-[#292929]">Feb 18, 2023</td>
+                <td className="py-3 text-center font-Lato text-[16px] font-normal text-[#292929]">Automatic</td>
                 <td className="py-3 text-center opacity-0 transition-opacity duration-200 group-hover:opacity-100">
                   <RxCross1 className="cursor-pointer text-[#292929]" />
                 </td>
