c86436f0f6810d946c941551c3f3c37c6616c290
Nitin Pokhriyal <32007748+NitinPokhriyal30@users.noreply.github.com>
2023-08-23 22:09:03 +0530

    Update AreaChart.jsx

--- src/components/Analytics/AreaChart.jsx
+++ src/components/Analytics/AreaChart.jsx
@@ -1,107 +1,100 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import * as d3 from 'd3';
-import  './AreaStyle.css';
+import './AreaStyle.css'
 
 const AreaChart = ({ data }) => {
-  const chartRef = useRef();
+  const svgRef = useRef();
 
   useEffect(() => {
-    if (data.length === 0) return;
-  
+    const svg = d3.select(svgRef.current);
     const margin = { top: 20, right: 30, bottom: 30, left: 40 };
-    const width = 550 - margin.left - margin.right;
-    const height = 350 - margin.top - margin.bottom;
-  
-    // Aggregate data on a weekly basis
-    const aggregatedData = d3.rollup(
-      data,
-      v => d3.mean(v, d => d.value), // Calculate the average value per week
-      d => d3.timeWeek.floor(d.date) // Group data by week
-    );
-  
-    const weeklyData = Array.from(aggregatedData, ([key, value]) => ({ date: new Date(key), value }));
-  
-    // Add an extra data point with the value 0 and date one week before the first date
-    const firstDate = weeklyData[0].date;
-    const oneWeekBeforeFirstDate = d3.timeWeek.offset(firstDate, -1);
-    const zeroDataPoint = { date: oneWeekBeforeFirstDate, value: 0 };
-    weeklyData.unshift(zeroDataPoint);
-  
-    const x = d3.scaleUtc()
-      .domain(d3.extent(weeklyData, d => d.date))
-      .range([0, width]);
-  
-    const y = d3.scaleLinear()
-      .domain([0, 40]) // Set the y-axis domain from 0 to 40
-      .range([height, 0])
-  
-    const area = d3.area()
-      .x(d => x(d.date))
-      .y0(height)
-      .y1(d => y(d.value));
-  
-    const svg = d3.select(chartRef.current)
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-      .style('overflow', 'hidden')
-      .attr('transform', `translate(${margin.left},${margin.top})`);
-  
+    // const parentWidth = svgRef.current.clientWidth; // Get the width of the container
+    const parentHeight = 340;
+    const width = 620;
+    const height = parentHeight;
+
+    const parseDate = d3.utcParse('%Y-%b-%d'); // Parse the date string
+    const formatDate = d3.timeFormat('%b %d');
+
+    // Add a data point with value 0 and date before the start date
+    const startDate = parseDate(data[0]?.date_range.split(' - ')[0]);
+    const zeroDataPoint = { date_range: `${formatDate(d3.timeDay.offset(startDate, -1))} - ${formatDate(startDate)}`, transaction_count: 0 };
+    const modifiedData = [zeroDataPoint, ...data];
+
+    const xData = modifiedData.map(d => formatDate(parseDate(d.date_range.split(' - ')[0])) + ' - ' + formatDate(parseDate(d.date_range.split(' - ')[1])));
+
+    { console.log(xData); }
+    const x = d3
+      .scaleBand()
+      .domain(xData)
+      .range([margin.left, width - margin.right - (width / modifiedData.length)]); // Adjust the range
+
+    const y = d3
+      .scaleLinear()
+      .domain([0, 25])
+      .nice()
+      .range([height - margin.bottom, margin.top]);
+
+    const area = d3
+      .area()
+      .x0(d => x(formatDate(parseDate(d.date_range.split(' - ')[0])) + ' - ' + formatDate(parseDate(d.date_range.split(' - ')[1]))))
+      .y0(y(0))
+      .y1(d => y(d.transaction_count));
+
+    svg.selectAll('*').remove();
+
     svg.append('path')
-      .datum(weeklyData)
-      .attr('fill', '#4CDFE80D')
-      .attr('stroke', '#4CDFE8')
-      .attr('stroke-width', 1)
+      .datum(modifiedData)
+      .attr('fill', '#E5EDFB') // Set fill color to #4CDFE8
+      .attr('stroke', '#4CDFE8') // Set border line color to red
+      .attr('stroke-width', 1) // Adjust border line width
       .attr('d', area);
-  
-    const xAxis = d3.axisBottom(x)
-      .tickValues(weeklyData.map(d => d.date))
-      .tickFormat((d, i) => (i === 0 ? null : d3.utcFormat("%b %d")(d))); // Format the x-axis tick labels as desired (e.g., "Jun 24", ...), remove label for the first tick
-  
+
     svg.append('g')
-      .attr('transform', `translate(0,${height})`)
-      .call(xAxis)
-      .call(g => g.select('.domain').remove()) // Remove x-axis line
-      .call(g => g.selectAll('.tick line').remove()); // Remove x-axis ticks
-  
-    const yAxis = d3.axisLeft(y)
-      .tickValues(d3.ticks(0, 40, 9))
-  
+      .attr('transform', `translate(0,${height - margin.bottom})`)
+      .call(
+        d3.axisBottom(x)
+          .tickSize(0) // Adjust padding to avoid overlapping labels
+          .tickPadding(-5)
+
+          .tickFormat((d, i) => i === 0 ? '' : d)
+      )
+      .selectAll('text') // Select all tick labels
+      .style('text-anchor', 'end') // Align the labels to the end of ticks
+      .attr('transform', 'rotate(-45) translate(-20, -10)'); // Rotate labels at -45 degrees
+
+    // Customize y-axis tick values
     svg.append('g')
-      .call(yAxis)
-      .call(g => g.select('.domain').remove()) // Remove y-axis line
-      .selectAll('.tick line')
-      .attr('stroke', '#d9d9d9') // Set the color of the grid lines to gray
-      .attr('stroke-opacity', 0.4) // Set the opacity of the grid lines
+      .attr('transform', `translate(${margin.left},0)`)
+      .call(
+        d3
+          .axisLeft(y)
+          .tickValues([0, 5, 10, 15, 20, 25]) // Set your desired tick values here
+          .tickSize(0) // Remove tick lines
+      );
 
     // Add vertical grid lines
-    const xAxisGrid = d3.axisBottom(x)
-      .tickValues(weeklyData.map(d => d.date))
-      .tickSize(-height)
-      .tickFormat('');
-      
-      
     svg.append('g')
-      .attr('class', 'x-grid')
-      .attr('transform', `translate(0,${height})`)
-      .call(xAxisGrid)
-      .selectAll('.tick line')
-      .attr('stroke', '#d9d9d9') // Set the color of the vertical grid lines to gray
-      .attr('stroke-opacity', 0.5) // Set the opacity of the vertical grid lines 
+      .selectAll('line')
+      .data(modifiedData) // Skip the last data point
+      .enter()
+      .append('line')
+      .attr('x1', d => x(formatDate(parseDate(d.date_range.split(' - ')[0])) + ' - ' + formatDate(parseDate(d.date_range.split(' - ')[1]))))
+      .attr('x2', d => x(formatDate(parseDate(d.date_range.split(' - ')[0])) + ' - ' + formatDate(parseDate(d.date_range.split(' - ')[1]))))
+      .attr('y1', margin.top)
+      .attr('y2', height - margin.bottom)
+      .attr('stroke', '#D3D3D3'); // Set grid line color
 
-      
+    // Add horizontal reference line
+    svg.append('line')
+      .attr('x1', margin.left)
+      .attr('x2', width - margin.right - (width / modifiedData.length + 52))
+      .attr('y1', y(0)) // Set the desired value for the reference line
+      .attr('y2', y(0))
+      .attr('stroke', '#D3D3D3');
   }, [data]);
-  
-  
-  
-  
-    
-  
 
-  return (
-    <svg ref={chartRef}></svg>
-    
-  );
+  return <svg ref={svgRef} width={500} height={400} />;
 };
 
 export default AreaChart;
